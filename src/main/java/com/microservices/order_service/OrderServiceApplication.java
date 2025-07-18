52549dd1fc5a90dd2586d127e1c9e7179b4a6284
Dimitrijevs <dimitrijevgleb@gmail.com>
2025-07-18 14:30:12 +0300

    feign client

--- src/main/java/com/microservices/order_service/OrderServiceApplication.java
+++ src/main/java/com/microservices/order_service/OrderServiceApplication.java
@@ -2,8 +2,12 @@ package com.microservices.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+
+// Enables detection and usage of feign clients ( different url, port )
+@EnableFeignClients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/microservices/order_service/service/OrderService.java
+++ src/main/java/com/microservices/order_service/service/OrderService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.microservices.order_service.client.InventoryClient;
 import com.microservices.order_service.dto.OrderRequest;
 import com.microservices.order_service.dto.OrderResponse;
 import com.microservices.order_service.model.Order;
@@ -18,17 +19,23 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest) {
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        Order order = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .price(orderRequest.price())
-                .skuCode(orderRequest.skuCode())
-                .price(orderRequest.price())
-                .quantity(orderRequest.quantity())
-                .build();
-
-        orderRepository.save(order);
+        if (isInStock) {
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .skuCode(orderRequest.skuCode())
+                    .price(orderRequest.price())
+                    .quantity(orderRequest.quantity())
+                    .build();
+    
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not available");
+        }
     }
 
     public List<OrderResponse> showAll() {
